c842e1c22a6c24c0b3b2c845e60577feeaabc185
junkingboy <junkingboy@163.com>
2026-05-20 12:21:37 +0800

    [mod] 修改返回值区别调用层次

--- depends/Auth.py
+++ depends/Auth.py
@@ -8,7 +8,7 @@ from enums.StandardBusEnum import StandardBusinessEnum
 
 async def authentication(r: Request) -> tuple:
     _headers: str | None = r.headers.get("Authorization")
-    if not _headers: raise DivExcep(StandardBusinessEnum.FAIL.value[0], "请求头校验失败")
+    if not _headers: raise DivExcep(StandardBusinessEnum.FAIL.value[0], "请求头缺少参数")
     if not _headers.startswith("Bearer "): raise DivExcep(StandardBusinessEnum.FAIL.value[0], "Token 格式错误")
     _token: str = _headers[len("Bearer "):]
     _verify_res: tuple = await verify_access_token(_token)
